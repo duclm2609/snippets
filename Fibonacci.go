b3761441738f68111ad64f6c498a442aa8afc3c5
duclm <duclm@vimo.vn>
2019-08-20 14:23:32 +0700

    Fix typo

--- Fibonacci.go
+++ Fibonacci.go
@@ -1,8 +1,14 @@
+package main
+
 func fibonacci(length int) {
 	f, g := 0, 1
 	for i := 0; i < length; i++ {
 		println(f)
 		f = f + g
-		f = f - g
+		g = f - g
 	}
+}
+
+func main() {
+	fibonacci(10)
 }
